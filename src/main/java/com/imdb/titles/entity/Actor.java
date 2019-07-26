3e21904a74264dc57177c3bdb6282a8413cbf340
yib519 <maxime.moise@capitalone.com>
2019-07-25 20:10:01 -0400

    created first entity and added db config

---
+++ src/main/java/com/imdb/titles/entity/Actor.java
@@ -0,0 +1,55 @@
+package com.imdb.titles.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Actor {
+
+    @Id
+    private String actorId;
+
+    @Column(name = "PRIMARY_NAME")
+    private String primaryName;
+
+    @Column(name = "BIRTH_YEAR")
+    private Integer birthYear;
+
+    @Column(name = "DEATH_YEAR")
+    private Integer deathYear;
+
+    public String getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(String actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getPrimaryName() {
+        return primaryName;
+    }
+
+    public void setPrimaryName(String primaryName) {
+        this.primaryName = primaryName;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(Integer deathYear) {
+        this.deathYear = deathYear;
+    }
+
+}
