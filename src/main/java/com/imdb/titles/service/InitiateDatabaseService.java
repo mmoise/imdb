d0b12cae9bb1a1a658065fd6c59f32a17286dd0a
yib519 <maxime.moise@capitalone.com>
2019-07-26 12:21:49 -0400

    making service a bean

--- src/main/java/com/imdb/titles/service/InitiateDatabaseService.java
+++ src/main/java/com/imdb/titles/service/InitiateDatabaseService.java
@@ -3,7 +3,9 @@ package com.imdb.titles.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InitiateDatabaseService implements CommandLineRunner {
 
     @Autowired
