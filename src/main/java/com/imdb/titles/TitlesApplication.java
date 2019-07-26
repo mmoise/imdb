ff3ab0cc0556b1dc9a5db62bae9b5d46b997765e
yib519 <maxime.moise@capitalone.com>
2019-07-25 20:01:32 -0400

    initial commit

---
+++ src/main/java/com/imdb/titles/TitlesApplication.java
@@ -0,0 +1,13 @@
+package com.imdb.titles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imdb/titles/TitlesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imdb.titles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TitlesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
