c3c5de8133c444516f99dc8ad1089d5435d28675
yib519 <maxime.moise@capitalone.com>
2019-07-25 20:47:23 -0400

    adding RestController annotation

--- src/main/java/com/imdb/titles/TitlesApplication.java
+++ src/main/java/com/imdb/titles/TitlesApplication.java
@@ -8,16 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
 @SpringBootApplication
+@RestController
 public class TitlesApplication {
 
 	@Autowired
 	private TitleService titleService;
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/titles")
 	Title test() {
 		Actor actor = new Actor();
 		actor.setActorId("actor1");
