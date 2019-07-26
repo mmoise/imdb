ae07c381f810a4ad479cbdf901bcc6e7bfe8e6e9
yib519 <maxime.moise@capitalone.com>
2019-07-25 21:17:55 -0400

    Adding Episode to test

--- src/main/java/com/imdb/titles/TitlesApplication.java
+++ src/main/java/com/imdb/titles/TitlesApplication.java
@@ -1,8 +1,11 @@
 package com.imdb.titles;
 
 import com.imdb.titles.entity.Actor;
+import com.imdb.titles.entity.Episode;
+import com.imdb.titles.entity.EpisodeId;
 import com.imdb.titles.entity.Title;
 import com.imdb.titles.service.ActorService;
+import com.imdb.titles.service.EpisodeService;
 import com.imdb.titles.service.TitleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -22,6 +25,9 @@ public class TitlesApplication {
 	@Autowired
 	private ActorService actorService;
 
+	@Autowired
+	private EpisodeService episodeService;
+
 	@RequestMapping(value = "/titles")
 	Title test() {
 
@@ -30,11 +36,23 @@ public class TitlesApplication {
 		actor.setPrimaryName("Tom Cruise");
 		actorService.save(actor);
 
+		EpisodeId episodeId = new EpisodeId();
+		episodeId.setTitleId("title1");
+		episodeId.setEpisodeNumber(2);
+		episodeId.setSeasonNumber(1);
+
+		Episode episode = new Episode();
+		episode.setEpisodeId("episode1");
+		episode.setRating(3.5);
+		episode.setId(episodeId);
+		episodeService.save(episode);
+
 		Title title = new Title();
 		title.setId("title1");
-		title.setTitleType("movie");
+		title.setTitleType("tvSeries");
 		title.setRating(9.5);
 		title.setCast(Collections.singletonList(actor));
+		title.setEpisodes(Collections.singletonList(episode));
 
 		title = titleService.save(title);
 
